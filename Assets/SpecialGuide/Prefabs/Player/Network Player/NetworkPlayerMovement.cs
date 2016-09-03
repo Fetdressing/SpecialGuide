1f71959eabec15019f5971c3c59f1369d365000e
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2016-09-03 17:30:06 +0200

    First test with network

---
+++ Assets/SpecialGuide/Prefabs/Player/Network Player/NetworkPlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.Networking;
+
+public class NetworkPlayerMovement : NetworkBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        InputDevice device = InputManager.ActiveDevice;
+        transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(device.LeftStickX * 900.0f * Time.deltaTime, 0.0f));
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+}
