0106bf59f5a6f4d26acd07a17d7480772a11e456
Joakim Westgren <narkos.newb@gmail.com>
2016-09-04 01:00:23 +0200

    Fixed KeyMan air forces

--- Assets/SpecialGuide/Prefabs/KeyMan/KeyManAI.cs
+++ Assets/SpecialGuide/Prefabs/KeyMan/KeyManAI.cs
@@ -8,23 +8,49 @@ public class KeyManAI : MonoBehaviour {
     public float m_maxSpeed = 2.5f;
     public float m_accelerationRate = 500.0f;
 
+    // Ground Checking Stuff
+    public LayerMask m_whatIsGround;
+    public Transform m_groundCheck;
+    const float m_groundedRadius = .5f;
+    private bool m_isGrounded;
+
 
 	// Use this for initialization
 	void Start ()
     {
         m_rigidBody = transform.GetComponent<Rigidbody2D>();
+        m_isGrounded = false;
 	}
 	
 	void FixedUpdate ()
     {
-	    if(m_rigidBody.velocity.magnitude > m_maxSpeed)
+        m_isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_groundCheck.position, m_groundedRadius, m_whatIsGround);
+        for(int i = 0; i<colliders.Length; i++)
         {
-            m_rigidBody.velocity = m_rigidBody.velocity.normalized * m_maxSpeed;
+            if(colliders[i].gameObject != m_rigidBody.gameObject)
+            {
+                m_isGrounded = true;
+            }
+        }
+
+
+        if(m_isGrounded)
+        {
+            if (m_rigidBody.velocity.magnitude > m_maxSpeed)
+            {
+                m_rigidBody.velocity = m_rigidBody.velocity.normalized * m_maxSpeed;
+            }
+            else
+            {
+                m_rigidBody.AddForce(Vector2.right * m_accelerationRate * Time.deltaTime);
+            }
         }
         else
         {
-            m_rigidBody.AddForce(Vector2.right * m_accelerationRate * Time.deltaTime);
+
         }
+	    
 
 
 
