13b48f79ff0e13907e4fd5b7b5b2c3c590eb4604
Fetdressing <simon.home@hotmail.com>
2016-09-05 18:54:48 +0200

    mer fix med bird. buggig still

--- Assets/SpecialGuide/Scripts/AI/FlyingEnemy.cs
+++ Assets/SpecialGuide/Scripts/AI/FlyingEnemy.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FlyingEnemy : UnitBase {
+public class FlyingEnemy : MonoBehaviour {
     private Transform thisTransform;
     private Rigidbody2D thisRigidbody;
     private Vector2 thisPos;
@@ -10,8 +10,14 @@ public class FlyingEnemy : UnitBase {
     public Transform meshObject;
 
     private GameObject[] players;
-    public LayerMask playerLayermask;
+    public LayerMask targetLayermask;
     public float playerCheckDistance = 100;
+
+    private Vector2 startPos;
+
+    private Vector2 currPatrolPos; //positionen som denne rör sig mot atm, gäller när CircleSkies enumeratorn körs
+    private float distanceMaxXPatrol = 50;
+    private float distanceMaxYPatrol = 15;
 
     public float moveForce = 900.0f;
 
@@ -21,21 +27,17 @@ public class FlyingEnemy : UnitBase {
         players = GameObject.FindGameObjectsWithTag("Player");
         thisTransform = this.transform;
         thisRigidbody = thisTransform.GetComponent<Rigidbody2D>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if(target == null)
-        {
-            CheckPlayerForPlayer();
-        }
 
         thisPos = new Vector2(thisTransform.position.x, thisTransform.position.y);
-    }
+        startPos = thisPos;
+        currPatrolPos = thisPos;
 
-    void CheckPlayerForPlayer()
+        StartCoroutine(CircleSkies());
+    }
+	
+    void CheckForPlayer()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(thisPos, playerCheckDistance, playerLayermask);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(thisPos, playerCheckDistance, targetLayermask);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
@@ -43,5 +45,41 @@ public class FlyingEnemy : UnitBase {
                 target = colliders[i].transform;
             }
         }
+    }
+
+    IEnumerator CircleSkies()
+    {
+        while(target == null)
+        {
+            thisPos = new Vector2(thisTransform.position.x, thisTransform.position.y);
+            if (Vector2.Distance(currPatrolPos, thisPos) < 0.5f)
+            {
+                float x = Random.Range(-distanceMaxXPatrol, distanceMaxXPatrol);
+                float y = Random.Range(-distanceMaxYPatrol, distanceMaxYPatrol);
+
+                currPatrolPos = new Vector2(thisPos.x + x, thisPos.y + y);
+            }
+            Debug.Log(currPatrolPos.ToString());
+            Vector2 dirToTarget = (new Vector2(currPatrolPos.x, currPatrolPos.y) - thisPos).normalized;
+            thisRigidbody.AddForce(dirToTarget * moveForce * Time.deltaTime);
+
+            //CheckForPlayer();
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        StartCoroutine(DiveAttack(target));
+    }
+
+    IEnumerator DiveAttack(Transform t)
+    {
+        while(thisPos.y >= t.position.y && target != null)
+        {
+            thisPos = new Vector2(thisTransform.position.x, thisTransform.position.y);
+            Vector2 dirToTarget = (new Vector2(t.position.x, t.position.y) - thisPos).normalized;
+            thisRigidbody.AddForce(dirToTarget * moveForce * 1.5f * Time.deltaTime);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        StartCoroutine(CircleSkies());
     }
 }
